bb57f18e1c42175770e89b88fef6665beaa7d350
Robert Nguyen <robertnguyen@Strix-17-GL703GE-f1e73f56.localdomain>
2023-07-19 10:21:09 +0700

    Initialize styles, header

--- app.py
+++ app.py
@@ -7,6 +7,5 @@ app = Flask(__name__)
 app.register_blueprint(routes_page)
 
 
-
 if __name__ == '__main__':
     app.run(debug=True)
--- route.py
+++ route.py
@@ -1,14 +1,18 @@
 from flask import Blueprint, render_template, send_from_directory
 
-routes_page = Blueprint('simple_page', __name__, template_folder='templates')
+# Initialize new blueprint
+routes_page = Blueprint('routes_page', __name__, template_folder='templates')
 
 @routes_page.route('/')
 def index():
     return render_template('home.html')
 
 
-
-# Serve asset files
+# Serve static files
 @routes_page.route('/assets/<path:path>')
-def send_report(path):
+def send_assets(path):
     return send_from_directory('templates/assets', path)
+
+@routes_page.route('/styles/<path:path>')
+def send_styles(path):
+    return send_from_directory('templates/styles', path)
